34a2f912f0d161e6652b8030bca45d749fec8aef
Ping Zhang <pingzh@umich.edu>
2015-06-12 12:57:27 -0500

    Initial Commit

---
+++ LuckKitchen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LuckKitchen
+//
+//  Created by Ping Zhang on 6/12/15.
+//  Copyright (c) 2015 Ping Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
